b621e411a5c57d5fd54881ff341c24f0cc833ef3
Sam Brightman <sam.brightman@gmail.com>
2018-01-11 12:59:17 -0500

    Clean up startup script

--- nginx-ldap-auth-daemon-ctl-rh.sh
+++ nginx-ldap-auth-daemon-ctl-rh.sh
@@ -7,23 +7,30 @@ if [ ! -f "$CMD" ]; then
 fi
 
 CMD=$PWD/$CMD
-PIDFILE=./nginx-ldap-auth-daemon.pid
+PIDFILE=$(basename $CMD .py).pid
+OUTFILE=$(basename $CMD .py).out
+ERRFILE=$(basename $CMD .py).err
 
 . /etc/init.d/functions
 
 start() {
     echo -n "Starting ldap-auth-daemon: "
     if [ -s ${PIDFILE} ]; then
-       RETVAL=1
-       echo -n "Already running !" && warning
-       echo
+        PID=$(cat ${PIDFILE})
+        if kill -0 ${PID} 2>/dev/null; then
+            echo -n "Already running!" && warning
+            echo
+        else
+            echo -n "Stale PID!" && failure
+            echo
+        fi
     else
-       nohup ${CMD} >/dev/null 2>&1 &
-       RETVAL=$?
-       PID=$!
-       [ $RETVAL -eq 0 ] && success || failure
-       echo
-       echo $PID > ${PIDFILE}
+        nohup ${CMD} >${OUTFILE} 2>${ERRFILE} &
+        PID=$!
+        sleep 1
+        kill -0 ${PID} 2>/dev/null && success || failure
+        echo
+        kill -0 ${PID} 2>/dev/null && echo ${PID} > ${PIDFILE}
     fi
 }
 
